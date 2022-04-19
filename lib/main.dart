55cfe87b983d029522045be9b81270f3e04f1e7a
shugo <hi.shu5.go93@gmail.com>
2022-04-19 21:37:29 +0900

    ver0.7.0

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'shuご.',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         fontFamily: 'NotoSansJP',
